92355f7dbb8e706d3e08ae168a7db2145c424f08
Dimitri Staessens <dimitri@ouroboros.rocks>
2020-06-07 21:22:35 +0200

    lib: Complete retransmission logic
    This completes the retransmission (automated repeat-request,
    ARQ)logic, sending (delayed) ACK messages when needed.
    
    On deallocation, flows will ACK try to retransmit any remaining
    unacknowledged messages (unless the FRCTFLINGER flag is turned off;
    this is on by default). Applications can safely shut down as soon as
    everything is ACK'd (i.e. the current Delta-t run is done). The
    activity timeout is now passed to the IPCP for it to sleep before
    completing deallocation (and releasing the flow_id). That should be
    moved to the IRMd in due time.
    
    Signed-off-by: Dimitri Staessens <dimitri@ouroboros.rocks>

--- src/lib/timerwheel.c
+++ src/lib/timerwheel.c
@@ -28,6 +28,8 @@
 #define RXMQ_SLOTS (1 << RXMQ_S)
 #define RXMQ_MAX   (1 << RXMQ_M)      /* us                       */
 
+#define ACKQ_SLOTS (1 << 10)          /* #slots for delayed ACK   */
+
 /* Overflow limits range to about 6 hours. */
 #define ts_to_ns(ts) (ts.tv_sec * BILLION + ts.tv_nsec)
 #define ts_to_slot(ts) ((ts_to_ns(ts) >> RXMQ_R) & (RXMQ_SLOTS - 1))
@@ -38,87 +40,105 @@ struct rxm {
         struct shm_du_buff * sdb;
         uint8_t *            head;
         uint8_t *            tail;
-        time_t               t0;    /* Time when original was sent (us). */
-        size_t               mul;   /* RTO multiplier.                   */
+        time_t               t0;      /* Time when original was sent (us). */
+        size_t               mul;     /* RTO multiplier.                   */
         struct frcti *       frcti;
+        int                  fd;
+        int                  flow_id; /* Prevent rtx when fd reused      */
 };
 
-struct rxmwheel {
-        struct list_head wheel[RXMQ_SLOTS];
+struct ack {
+        struct list_head next;
+        struct frcti *   frcti;
+        int              fd;
+        int              flow_id;
+};
+
+struct {
+        struct list_head rxms[RXMQ_SLOTS];
+        struct list_head acks[RXMQ_SLOTS];
+        bool             map[RXMQ_SLOTS][PROG_MAX_FLOWS];
 
         size_t           prv; /* Last processed slot. */
         pthread_mutex_t  lock;
-};
+} rw;
 
-static void rxmwheel_destroy(struct rxmwheel * rw)
+static void timerwheel_fini(void)
 {
         size_t             i;
         struct list_head * p;
         struct list_head * h;
 
-        pthread_mutex_destroy(&rw->lock);
+        pthread_mutex_lock(&rw.lock);
 
         for (i = 0; i < RXMQ_SLOTS; ++i) {
-                list_for_each_safe(p, h, &rw->wheel[i]) {
+                list_for_each_safe(p, h, &rw.rxms[i]) {
                         struct rxm * rxm = list_entry(p, struct rxm, next);
                         list_del(&rxm->next);
                         shm_du_buff_ack(rxm->sdb);
                         ipcp_sdb_release(rxm->sdb);
                         free(rxm);
                 }
+
+                list_for_each_safe(p, h, &rw.acks[i]) {
+                        struct ack * a = list_entry(p, struct ack, next);
+                        list_del(&a->next);
+                        free(a);
+                }
         }
+
+        pthread_mutex_unlock(&rw.lock);
+
+        pthread_mutex_destroy(&rw.lock);
 }
 
-static struct rxmwheel * rxmwheel_create(void)
+static int timerwheel_init(void)
 {
-        struct rxmwheel * rw;
         struct timespec   now;
         size_t            i;
 
-        rw = malloc(sizeof(*rw));
-        if (rw == NULL)
-                return NULL;
-
-        if (pthread_mutex_init(&rw->lock, NULL)) {
-                free(rw);
-                return NULL;
-        }
+        if (pthread_mutex_init(&rw.lock, NULL))
+                return -1;
 
         clock_gettime(PTHREAD_COND_CLOCK, &now);
 
         /* Mark the previous timeslot as the last one processed. */
-        rw->prv = (ts_to_slot(now) - 1) & (RXMQ_SLOTS - 1);
+        rw.prv = (ts_to_slot(now) - 1) & (RXMQ_SLOTS - 1);
 
-        for (i = 0; i < RXMQ_SLOTS; ++i)
-                list_head_init(&rw->wheel[i]);
+        for (i = 0; i < RXMQ_SLOTS; ++i) {
+                list_head_init(&rw.rxms[i]);
+                list_head_init(&rw.acks[i]);
+        }
 
-        return rw;
+        return 0;
 }
 
-static void rxmwheel_move(struct rxmwheel * rw)
+static void timerwheel_move(void)
 {
         struct timespec    now;
         struct list_head * p;
         struct list_head * h;
         size_t             slot;
         size_t             i;
+        size_t             j;
 
-        pthread_mutex_lock(&rw->lock);
+        pthread_mutex_lock(&rw.lock);
 
         pthread_cleanup_push((void (*) (void *)) pthread_mutex_unlock,
-                             (void *) &rw->lock);
+                             (void *) &rw.lock);
 
         clock_gettime(PTHREAD_COND_CLOCK, &now);
 
         slot = ts_to_slot(now);
 
-        i = rw->prv;
+        i = rw.prv;
+        j = rw.prv;
 
         if (slot < i)
                 slot += RXMQ_SLOTS;
 
         while (i++ < slot) {
-                list_for_each_safe(p, h, &rw->wheel[i & (RXMQ_SLOTS - 1)]) {
+                list_for_each_safe(p, h, &rw.rxms[i & (RXMQ_SLOTS - 1)]) {
                         struct rxm *         r;
                         struct frct_cr *     snd_cr;
                         struct frct_cr *     rcv_cr;
@@ -127,7 +147,6 @@ static void rxmwheel_move(struct rxmwheel * rw)
                         struct shm_du_buff * sdb;
                         uint8_t *            head;
                         struct flow *        f;
-                        int                  fd;
                         uint32_t             snd_lwe;
                         uint32_t             rcv_lwe;
                         time_t               rto;
@@ -138,20 +157,21 @@ static void rxmwheel_move(struct rxmwheel * rw)
 
                         snd_cr = &r->frcti->snd_cr;
                         rcv_cr = &r->frcti->rcv_cr;
-                        fd     = r->frcti->fd;
-                        f      = &ai.flows[fd];
+                        f      = &ai.flows[r->fd];
 
                         shm_du_buff_ack(r->sdb);
+
+                        if (f->frcti == NULL || f->flow_id != r->flow_id) {
+                                ipcp_sdb_release(r->sdb);
+                                free(r);
+                                continue;
+                        }
 
                         pthread_rwlock_wrlock(&r->frcti->lock);
 
                         snd_lwe = snd_cr->lwe;
                         rcv_lwe = rcv_cr->lwe;
                         rto     = r->frcti->rto;
-                        /* Assume last RTX is the one that's ACK'd. */
-                        if (r->frcti->probe
-                            && (r->frcti->rttseq + 1) == r->seqno)
-                                r->frcti->t_probe = now;
 
                         pthread_rwlock_unlock(&r->frcti->lock);
 
@@ -170,6 +190,10 @@ static void rxmwheel_move(struct rxmwheel * rw)
                                 shm_rbuff_set_acl(f->tx_rb, ACL_FLOWDOWN);
                                 continue;
                         }
+
+                        if (r->frcti->probe
+                            && (r->frcti->rttseq + 1) == r->seqno)
+                                r->frcti->probe = false;
 
                         /* Copy the payload, safe rtx in other layers. */
                         if (ipcp_sdb_reserve(&sdb, r->tail - r->head)) {
@@ -206,24 +230,45 @@ static void rxmwheel_move(struct rxmwheel * rw)
                         r->head = head;
                         r->tail = shm_du_buff_tail(sdb);
                         r->sdb  = sdb;
+                        r->mul++;
 
                         /* Schedule at least in the next time slot */
-                        rslot = (slot + MAX((rto >> RXMQ_R), 1))
+                        rslot = (slot + MAX(((rto * r->mul) >> RXMQ_R), 1))
                                 & (RXMQ_SLOTS - 1);
 
-                        list_add_tail(&r->next, &rw->wheel[rslot]);
+                        list_add_tail(&r->next, &rw.rxms[rslot]);
                 }
         }
 
-        rw->prv = slot & (RXMQ_SLOTS - 1);
+        while (j++ < slot) {
+                list_for_each_safe(p, h, &rw.acks[j & (ACKQ_SLOTS - 1)]) {
+                        struct ack *  a;
+                        struct flow * f;
+
+                        a = list_entry(p, struct ack, next);
+
+                        list_del(&a->next);
+
+                        f = &ai.flows[a->fd];
+
+                        rw.map[j & (ACKQ_SLOTS - 1)][a->fd] = false;
+
+                        if (f->flow_id == a->flow_id && f->frcti != NULL)
+                                frct_send_ack(a->frcti);
+
+                        free(a);
+
+                }
+        }
+
+        rw.prv = slot & (RXMQ_SLOTS - 1);
 
         pthread_cleanup_pop(true);
 }
 
-static int rxmwheel_add(struct rxmwheel *    rw,
-                        struct frcti *       frcti,
-                        uint32_t             seqno,
-                        struct shm_du_buff * sdb)
+static int timerwheel_rxm(struct frcti *       frcti,
+                          uint32_t             seqno,
+                          struct shm_du_buff * sdb)
 {
         struct timespec now;
         struct rxm *    r;
@@ -247,15 +292,53 @@ static int rxmwheel_add(struct rxmwheel *    rw,
 
         slot = (((r->t0 + frcti->rto) >> RXMQ_R) + 1) & (RXMQ_SLOTS - 1);
 
+        r->fd      = frcti->fd;
+        r->flow_id = ai.flows[r->fd].flow_id;
+
         pthread_rwlock_unlock(&r->frcti->lock);
 
-        pthread_mutex_lock(&rw->lock);
+        pthread_mutex_lock(&rw.lock);
 
-        list_add_tail(&r->next, &rw->wheel[slot]);
+        list_add_tail(&r->next, &rw.rxms[slot]);
 
         shm_du_buff_wait_ack(sdb);
 
-        pthread_mutex_unlock(&rw->lock);
+        pthread_mutex_unlock(&rw.lock);
+
+        return 0;
+}
+
+static int timerwheel_ack(int            fd,
+                          struct frcti * frcti)
+{
+        struct timespec now;
+        struct ack *    a;
+        size_t          slot;
+
+        a = malloc(sizeof(*a));
+        if (a == NULL)
+                return -ENOMEM;
+
+        clock_gettime(PTHREAD_COND_CLOCK, &now);
+
+        slot = (((ts_to_ns(now) + DELT_ACK) >> RXMQ_R) + 1) & (ACKQ_SLOTS - 1);
+
+        a->fd    = fd;
+        a->frcti = frcti;
+        a->flow_id = ai.flows[fd].flow_id;
+
+        pthread_mutex_lock(&rw.lock);
+
+        if (rw.map[slot][fd]) {
+                pthread_mutex_unlock(&rw.lock);
+                return 0;
+        }
+
+        rw.map[slot][fd] = true;
+
+        list_add_tail(&a->next, &rw.acks[slot]);
+
+        pthread_mutex_unlock(&rw.lock);
 
         return 0;
 }
